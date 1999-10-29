f8c9993fdd86e39c200dc50154df49d35a16c90d
cvs2git <>
1999-10-29 08:06:43 +0000

    This commit was manufactured by cvs2svn to create tag 'OX_MATH_1_0'.

--- src/kan96xx/plugin/asir.h
+++
@@ -1,8 +0,0 @@
-int Asir_ExecuteString(char *s);
-char *Asir_PopString(void);
-char *KasirKanConvert(char *s);
-
-int Asir_Set(char *name);
-int Asir_PushBinary(int size,void *data);
-void *Asir_PopBinary(int *sizep);
-
