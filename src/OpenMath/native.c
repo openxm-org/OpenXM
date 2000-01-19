f2de9d77286996116fc54931b898fbd8ab10a4a2
tam <>
2000-01-19 10:00:10 +0000

    I forgot to remove C code.

--- src/OpenMath/native.c
+++
@@ -1,8 +0,0 @@
-#include <signal.h>
-#include <unistd.h>
-#include "OMproxy.h"
-
-JNIEXPORT void JNICALL Java_OMproxy_setNoInterrupt(JNIEnv *env, jobject obj){
-  signal(SIGINT, SIG_IGN);
-  return;
-}
