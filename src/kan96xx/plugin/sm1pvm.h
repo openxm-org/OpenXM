d36c2b34da1062e0f3a9ec34dd55de8261cd77db
cvs2git <>
2000-01-20 08:52:47 +0000

    This commit was manufactured by cvs2svn to create tag 'SPECS_20000120'.

--- src/kan96xx/plugin/sm1pvm.h
+++
@@ -1,6 +0,0 @@
-/* $OpenXM$ */
-int KpvmStartSlaves(char *name,int n);
-int KpvmStopSlaves(void);
-int KpvmChangeStateOfSlaves(int k);
-int KpvmMcast(char *comm);
-struct object KpvmJobPool(struct object obj);
