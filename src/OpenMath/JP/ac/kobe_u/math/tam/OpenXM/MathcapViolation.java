167cdd916efd923cddc1d803641eedbff3c853b8
tam <>
2000-01-20 16:38:36 +0000

    I added a exception of MathcapViolation.

---
+++ src/OpenMath/JP/ac/kobe_u/math/tam/OpenXM/MathcapViolation.java
@@ -0,0 +1,14 @@
+/**
+ * $OpenXM$
+ */
+package JP.ac.kobe_u.math.tam.OpenXM;
+
+import java.io.*;
+
+public class MathcapViolation extends Exception{
+  public MathcapViolation(){}
+
+  public MathcapViolation(String s){
+    super(s);
+  }
+}
