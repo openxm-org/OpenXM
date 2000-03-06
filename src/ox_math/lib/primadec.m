138de6b89f7eeb0a9343225cc1b6078827db37e9
ohara <>
2000-03-06 09:59:50 +0000

    primadec.m supports multi ox connections.

--- src/ox_math/lib/primadec.m
+++ src/ox_math/lib/primadec.m
@@ -1,4 +1,4 @@
-(* $OpenXM: OpenXM/src/ox_math/lib/primadec.m,v 1.2 1999/12/17 02:21:41 ohara Exp $ *)
+(* $OpenXM: OpenXM/src/ox_math/lib/primadec.m,v 1.3 2000/01/20 15:00:02 ohara Exp $ *)
 
 (*
 usage:
@@ -8,17 +8,19 @@ Copyright 1988-97 Wolfram Research, Inc.
  -- Motif graphics initialized -- 
 
 In[1]:= <<primadec.m
-In[2]:= OxStart["ox_asir"]
-In[3]:= primadec[{x^2-3*x+2},{x}]
-Out[3]= {{{-1 + x}, {-1 + x}}, {{-2 + x}, {-2 + x}}}
+In[2]:= primadec[{x^2-3*x+2},{x}]
+Out[2]= {{{-1 + x}, {-1 + x}}, {{-2 + x}, {-2 + x}}}
 *)
 
 primadec[polys_List, vars_List] := Block[
-	{s},
+	{s, expr, fd},
 	s = StringJoin[
 		{"m_rtostr(primadec( ", asirRtostr[polys], " , ",  asirRtostr[vars], " ));"}];
-	OxExecuteString[s];
-	ToExpression[OxPopString[]]
+	fd = OxStart["ox_asir"];
+	OxExecuteString[fd, s];
+	expr = ToExpression[OxPopString[fd]];
+	OxClose[fd];
+	Return[expr]
 	]
 
 Get[Environment["OpenXM_HOME"] <> "/lib/math/oxclient.m"]
