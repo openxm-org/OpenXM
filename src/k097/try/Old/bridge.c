713509b20d98292ea1cef5a1c2d34560587273ef
cvs2git <>
2000-09-25 08:21:53 +0000

    This commit was manufactured by cvs2svn to create tag 'PLIST'.

--- src/k097/try/Old/bridge.c
+++
@@ -1,6 +0,0 @@
-
-
-void GC_init__Fv(void) { GC_init(); }
-int KSexecuteString__FPc(char *s) { return KSexecuteString(s); }
-void KSstart__Fv(void) { KSstart(); }
-
--- src/kan96xx/Kan/kanlib.c
+++
@@ -1,7 +0,0 @@
-/* $OpenXM$ */
-/* kanlib.c */
-char *kanlib_Copyright() {
-  return("This is kanlib.a. This library may be freely distributed and used as is with no warranty expressed.");
-}
-
-  
