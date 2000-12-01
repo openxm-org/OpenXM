a19b6b0ec7e5eab5424a8c9a622cebf8cf1e8c3a
maekawa <>
2000-12-01 08:09:41 +0000

    Add $OpenXM$

--- src/ox_socket/ox_getport.c
+++ src/ox_socket/ox_getport.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Maekawa: ox_getport.c,v 1.17 2000/11/21 13:38:51 maekawa Exp $
+ *	$OpenXM$
  */
 
 #include <sys/types.h>
