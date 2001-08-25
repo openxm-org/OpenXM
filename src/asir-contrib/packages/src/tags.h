862257971eb53d5b4564fa3c10aec27846146036
takayama <>
2001-08-25 04:39:04 +0000

    New datatypes are added by using struct (experimental). See datatype.rr. base_field : field. base_identity : identity. base_text :  text which explains the meaning of identity. poly_factored_polynomial :  polynomial in factored form. poly_factored_rational   : rational in factored form. poly_polynomial          : polynomial that belongs to a ring. poly_ideal               : ideal poly_ring                : ring

---
+++ src/asir-contrib/packages/src/tags.h
@@ -0,0 +1,24 @@
+/* $OpenXM$ */
+
+#define NUMBER 1
+#define RPOLYNOMIAL 2
+#define RATIONAL 3
+#define LIST 4
+#define VECTOR 5
+#define MATRIX 6
+#define STRING 7
+#define STRUCT 8
+#define DPOLYNOMIAL   9
+#define QUOTE 17
+
+
+#define BASE_FIELD               "base_field"
+#define BASE_IDENTITY            "base_identity"
+#define BASE_RULE                "base_rule"
+#define BASE_TEXT                "base_text"
+
+#define POLY_FACTORED_POLYNOMIAL "poly_factored_polynomial"
+#define POLY_FACTORED_RATIONAL   "poly_factored_rational"
+#define POLY_RING                "poly_ring"
+#define POLY_POLYNOMIAL          "poly_polynomial"
+#define POLY_IDEAL               "poly_ideal"
