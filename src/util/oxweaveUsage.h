4dd872a4d36cdba9b4a9269551223d33b362e01f
takayama <>
2002-04-05 01:12:09 +0000

    Created util directory, which now contains oxweave document generation system.  It is installed to OpenXM/bin.

---
+++ src/util/oxweaveUsage.h
@@ -0,0 +1,24 @@
+/* OpenXM: OpenXM/src/kxx/oxweaveUsage.h,v 1.3 1999/12/15 03:30:14 takayama Exp
+   $OpenXM$
+*/
+
+  fprintf(stderr,"oxweave [--plain] [--source] [--recursive] [key1 key2 ...]\n");
+  fprintf(stderr,"oxweave reads bytes from the standarndard input stream \n");
+  fprintf(stderr,"and outputs them if one of the following conditions\n");
+  fprintf(stderr,"is satisfied:\n");
+  fprintf(stderr,"(1) hogehoge is output if hogehoge is enclosed \n");
+  fprintf(stderr,"    like /*&xyz  hogehoge */ and xyz matches with one of the keys\n");
+  fprintf(stderr,"(2) hogehoge is output if hogehoge comes after //&xyz \n");
+  fprintf(stderr,"    followed with the newline and xyz matches with one of the keys\n");
+  fprintf(stderr,"(3) If the option --plain is given, all texts out of /*&xyz */ and //&xyz \n");
+  fprintf(stderr,"    are output.\n");
+  fprintf(stderr,"(4) If the option --recursive is given, the symbol */ is treated recursively.\n");
+  fprintf(stderr,"(5) --debug is used to find unmatched /* and */\n");
+  fprintf(stderr,"Example 1: oxweave --source jp <oxweave.c >t.tex\n");
+  fprintf(stderr,"Example 2: oxweave  jp <oxweave.c >t.tex\n");
+  fprintf(stderr,"Example 3: oxweave --plain  <oxweave.c >t.tex\n");
+
+
+
+
+
