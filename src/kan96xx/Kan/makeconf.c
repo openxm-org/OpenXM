0f375b744197d60aeba4feab3bfffe809cbf770b
takayama <>
2002-10-24 06:04:07 +0000

    Fixed one more bug on Solaris.

--- src/kan96xx/Kan/makeconf.c
+++ src/kan96xx/Kan/makeconf.c
@@ -1,10 +1,10 @@
-/* $OpenXM: OpenXM/src/kan96xx/Kan/makeconf.c,v 1.1 2001/05/20 07:48:19 takayama Exp $ */
+/* $OpenXM: OpenXM/src/kan96xx/Kan/makeconf.c,v 1.2 2002/10/24 05:53:37 takayama Exp $ */
 #include <stdio.h>
 main() {
 #if defined(__CYGWIN__)
   printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.a/\n");
 #elif defined(sun)
-  printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.so -L $(OpenXM_HOME)\\/lib/\n");
+  printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.a -L $(OpenXM_HOME)\\/lib/\n");
 #else
   printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.so -Wl,--rpath -Wl,$(OpenXM_HOME)\\/lib/\n");
 #endif
