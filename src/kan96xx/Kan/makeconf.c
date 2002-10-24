99cea1f7e7d427c33f50cc9b7822463450de5521
takayama <>
2002-10-24 05:53:37 +0000

    Fixed a bug to generate Makefile on Solaris.

--- src/kan96xx/Kan/makeconf.c
+++ src/kan96xx/Kan/makeconf.c
@@ -1,8 +1,10 @@
-/* $OpenXM$ */
+/* $OpenXM: OpenXM/src/kan96xx/Kan/makeconf.c,v 1.1 2001/05/20 07:48:19 takayama Exp $ */
 #include <stdio.h>
 main() {
 #if defined(__CYGWIN__)
   printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.a/\n");
+#elif defined(sun)
+  printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.so -L $(OpenXM_HOME)\\/lib/\n");
 #else
   printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.so -Wl,--rpath -Wl,$(OpenXM_HOME)\\/lib/\n");
 #endif
