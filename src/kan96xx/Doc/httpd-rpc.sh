318edad64af212e3535530ed942b2468ac4aa1cb
takayama <>
2002-10-24 08:45:55 +0000

    Fixed a typo.

--- src/kan96xx/Doc/httpd-rpc.sh
+++ src/kan96xx/Doc/httpd-rpc.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $OpenXM$
+# $OpenXM: OpenXM/src/kan96xx/Doc/httpd-rpc.sh,v 1.1 2002/10/24 07:45:03 takayama Exp $
 LOGFILE=httpd-rpc-8090.log
 export LOGFILE
 OXWEB_POST=1
@@ -8,7 +8,7 @@ touch $LOGFILE
 tail -f $LOGFILE &
 while true ; \
 do \
-  echo "date" >>$LOGFILE ;
+  date >>$LOGFILE ;
   sm1 -s "[(parse) (httpd-rpc.sm1) pushfile] extension ; httpd ; quit " >> $LOGFILE 2>&1 ;
   sleep 3 ; \
 done
