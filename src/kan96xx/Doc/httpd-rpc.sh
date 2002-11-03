bf07b7cc2a5bd97ddfd58ac0a67745afdf53726c
takayama <>
2002-11-03 12:43:03 +0000

    Added a new function httpd.parse to parse GET and POST commands. Made several changes of httpd-rpc.sh for separating httpd-rpc.sm1 into two files.

--- src/kan96xx/Doc/httpd-rpc.sh
+++ src/kan96xx/Doc/httpd-rpc.sh
@@ -1,19 +1,42 @@
 #! /bin/sh
-# $OpenXM: OpenXM/src/kan96xx/Doc/httpd-rpc.sh,v 1.3 2002/10/29 10:55:19 takayama Exp $
-LOGFILE=httpd-rpc-8090.log
+# $OpenXM: OpenXM/src/kan96xx/Doc/httpd-rpc.sh,v 1.4 2002/10/30 13:23:06 takayama Exp $
+# OpenXM/rc/dot.bashrc must be read before execution of this script.
+# source ${OpenXM_HOME}/rc/dot.bashrc
+#
+# Output verbose messages  
+# OXWEB_DEBUG=1
+# export OXWEB_DEBUG
+#
+# Port number for the httpd service
+OXWEB_PORT=8090
+export OXWEB_PORT
+#
+# asirrc is executed when risa/asir starts. Override it if necessary.
+#ASIR_CONFIG=${OpenXM_HOME}/rc/asirrc
+#export ASIR_CONFIG
+#
+# Set your own httpd-rpc file if necessary.
+OXWEB_HTTPD_RPC=httpd-rpc.sm1
+export OXWEB_HTTPD_RPC
+#
+#
+#LOGFILE=httpd-rpc-${OXWEB_PORT}.log
+LOGFILE=/dev/tty
 export LOGFILE
 OXWEB_POST=1
 export OXWEB_POST
 OXWEB_TAKE_LOG=1
 export OXWEB_TAKE_LOG
-# OXWEB_DEBUG=1
-# export OXWEB_DEBUG
+#
 touch $LOGFILE
 # tail -f $LOGFILE &
+#
+#
+#  Main loop
 while true ; \
 do \
   date >>$LOGFILE ;
-  sm1 -s "[(parse) (httpd-rpc.sm1) pushfile] extension ; httpd ; quit " >> $LOGFILE 2>&1 ;
+  sm1 -s "[(parse) (${OXWEB_HTTPD_RPC}) pushfile] extension ; httpd ; quit " >> $LOGFILE 2>&1 ;
   sleep 3 ; \
 done
 
