e3aff9bdd87ba87c5040ec8665118dffd72b758f
takayama <>
2003-01-15 00:04:02 +0000

    Use -L${OpenXM}/lib -lgmp

--- src/kan96xx/Kan/makeconf.c
+++ src/kan96xx/Kan/makeconf.c
@@ -1,12 +1,12 @@
-/* $OpenXM: OpenXM/src/kan96xx/Kan/makeconf.c,v 1.2 2002/10/24 05:53:37 takayama Exp $ */
+/* $OpenXM: OpenXM/src/kan96xx/Kan/makeconf.c,v 1.3 2002/10/24 06:04:07 takayama Exp $ */
 #include <stdio.h>
 main() {
 #if defined(__CYGWIN__)
-  printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.a/\n");
+  printf("s/@@@LIBGMP@@@/ -L$(OpenXM_HOME)\\/lib  -lgmp /\n");
 #elif defined(sun)
-  printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.a -L $(OpenXM_HOME)\\/lib/\n");
+  printf("s/@@@LIBGMP@@@/ -L$(OpenXM_HOME)\\/lib -lgmp /\n");
 #else
-  printf("s/@@@LIBGMP@@@/$(OpenXM_HOME)\\/lib\\/libgmp.so -Wl,--rpath -Wl,$(OpenXM_HOME)\\/lib/\n");
+  printf("s/@@@LIBGMP@@@/ -L$(OpenXM_HOME)\\/lib -lgmp -Wl,--rpath -Wl,$(OpenXM_HOME)\\/lib/\n");
 #endif
 }
 
