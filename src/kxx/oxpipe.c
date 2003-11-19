f2207c3d432f251d83e077503ced3c16ba84da85
ohara <>
2003-11-19 10:14:07 +0000

    four execl are replaced by one execv

--- src/kxx/oxpipe.c
+++ src/kxx/oxpipe.c
@@ -1,6 +1,8 @@
-/* $OpenXM$ */
+/* $OpenXM: OpenXM/src/kxx/oxpipe.c,v 1.1 2003/11/19 00:43:18 takayama Exp $ */
 #include <stdio.h>
+#include <unistd.h>
 
+int
 main(int argc,char *argv[]) {
   dup2(0,3);
   dup2(1,4);
@@ -10,10 +12,6 @@ main(int argc,char *argv[]) {
   if (argc < 2) {
 	fprintf(stderr,"oxpipe servername\n");
   }
-
-  if (argc == 2) execl(argv[1],argv[1],NULL);
-  if (argc == 3) execl(argv[1],argv[1],argv[2],NULL);
-  if (argc == 4) execl(argv[1],argv[1],argv[2],argv[3],NULL);
-  if (argc == 5) execl(argv[1],argv[1],argv[2],argv[3],argv[4],NULL);
+  if (argc >=2 && argc <= 5) execv(argv[1], argv+1);
   if (argc > 5) {fprintf(stderr,"Too many args\n"); exit(10);}
 }
