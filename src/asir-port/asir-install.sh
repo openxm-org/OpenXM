68a143ad07348a61da0b3b35f4ae6a3ee3eab19f
takayama <>
2004-02-27 00:12:22 +0000

    asir-install.sh downloads asir (FLL parts) from ftp.math.kobe-u.ac.jp and installs under $OpenXM_HOME (not tested). ox-texmacs-$os.tar.gz is also downloaded and installed.

--- src/asir-port/asir-install.sh
+++ src/asir-port/asir-install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-# $OpenXM: OpenXM/src/asir-port/asir-port.sh,v 1.3 2004/02/24 05:43:14 takayama Exp $
+# $OpenXM$
 # Downloading Risa/Asir for FLL-free systems.
-# Risa/Asir is installed under $HOME/.asir-tmp/$asirname
-# Symbolic link to $asirname from $OpenXM_HOME/bin/asir must exist
-# in the distribution of FLL-free distribution.
-# Starting script of Risa/Asir may call this shell script.
+# Risa/Asir is installed under $OpenXM_HOME/bin
 os=`uname -s`
 md=`cat $OpenXM_HOME/lib/asir/distinfo-asir`
 libmd=`cat $OpenXM_HOME/lib/asir/distinfo-lib`
@@ -22,14 +19,19 @@ asirlibname="asirlib-$os-$libmd.tar.gz"
 ot="ftp://ftp.math.kobe-u.ac.jp/pub/asir/knoppix/ox-texmacs-$os.tar.gz"
 otname="ox-texmacs-$os.tar.gz"
 
+ASIR_LIB_FILES=alph bfct bgk const cyclic defs.h dmul fctrdata fctrtest \
+               fff gr ifplot katsura mat nf num primdec \
+               ratint robot solve sp sturm xm
+
 if [ $# = 1 ]; then
-if [ $1 = "--install" ]; then
+if [ $1 = "--force" ]; then
  rm -rf $HOME/.asir-tmp
 fi
 fi
 
 _agree() {
 	echo "------------------------------------------------------------------"
+	echo "This script install Risa/Asir under $OpenXM_HOME"
 	echo "Risa/Asir is distributed with no warranty for non-commercial use."
 	echo "OpenXM subcomponents are distributed with no warranty under BSD license or GPL."
 	echo "Do you agree with the licenses under $OpenXM_HOME/Copyright?"
@@ -51,15 +53,6 @@ _agree() {
 	_agree
 }
 
-# If there is un usb memory, then download under the usb memory.
-if [ -d /mnt/sda1/.asir-tmp ] ; then
-	rm -f $HOME/.asir-tmp ; \
-	ln -s /mnt/sda1/.asir-tmp $HOME/.asir-tmp 
-fi
-if [ -d /mnt/sdb1/.asir-tmp ] ; then
-	rm -f $HOME/.asir-tmp ; \
-	ln -s /mnt/sdb1/.asir-tmp $HOME/.asir-tmp
-fi
 
 if [ ! -f $HOME/.asir-tmp/$asirnamegunzip ]; then
 	_agree ; \
@@ -68,23 +61,25 @@ if [ ! -f $HOME/.asir-tmp/$asirnamegunzip ]; then
 	echo "Done." ; \
 	gunzip $HOME/.asir-tmp/$asirname ; \
 	chmod +x $HOME/.asir-tmp/$asirnamegunzip ; \
-	rm -f $HOME/.asir-tmp/asir ; \
-	ln -s $HOME/.asir-tmp/$asirnamegunzip $HOME/.asir-tmp/asir ; \
+	install -m 755 $HOME/.asir-tmp/$asirnamegunzip $OpenXM_HOME/bin/asir ; \
 fi
 if [ ! -f $HOME/.asir-tmp/$asirlibname ]; then
 	echo -n "Downloading the asir library  $asirlib... " ; \
 	oxfetch.sh $asirlib $HOME/.asir-tmp ; \
 	echo "Done." ; \
 	(cd $HOME/.asir-tmp ; tar xzf $asirlibname) ; \
+	(cd $OpenXM_HOME/lib/asir ; rm -f $ASIR_LIB_FILES) ; \
+	(cd $HOME/.asir-tmp ; install -m 644 $ASIR_LIB_FILES $OpenXM_HOME/lib/asir) ; \
 fi
 if [ ! -f $HOME/.asir-tmp/$otname ]; then
-	echo -n "Downloading $ot plugin... " ; \
+	echo -n "Downloading the $ot plugin... " ; \
 	oxfetch.sh $ot $HOME/.asir-tmp ; \
 	echo "Done." ; \
 	(cd $HOME ; tar xzf $HOME/.asir-tmp/$otname) ; \
 fi
 
 
-$OpenXM_HOME/bin/fep $OpenXM_HOME/bin/asir $*
+echo "Installation is completed."
+
 
 
