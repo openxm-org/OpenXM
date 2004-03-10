af65b7af8e7fccbf43f1f76441b74c2279a1906a
ohara <>
2004-03-10 17:31:13 +0000

    (temporary work) Define a function "my-openxm-plugin-eval-paste" but it is incomplete.

--- src/kxx/init-openxm.scm
+++ src/kxx/init-openxm.scm
@@ -1,8 +1,16 @@
-; $OpenXM: OpenXM/src/kxx/init-openxm.scm,v 1.1 2004/03/05 19:31:12 ohara Exp $
+; $OpenXM: OpenXM/src/kxx/init-openxm.scm,v 1.2 2004/03/07 22:55:12 ohara Exp $
+
+(define (my-openxm-plugin-eval t)
+  (import-from (texmacs plugin plugin-cmd))
+  (import-from (texmacs plugin plugin-convert))
+  (plugin-eval "openxm" "default" (object->tree t)))
+
+(define (my-openxm-plugin-eval-paste t)
+  (insert-tree (object->tree (my-openxm-plugin-eval t))))
 
 (define (w3m t)
   (if (string? t)
-	(system (string-append "rxvt -g 100x50 -e w3m " t "&"))))
+    (system (string-append "rxvt -g 100x50 -e w3m " t "&"))))
 
 (define (w3m-local t)
   (if (string? t)
@@ -11,22 +19,26 @@
 (define (openxm-initialize)
   (menu-extend texmacs-session-help-icons
     (if (and (in-openxm?)
-         (url-exists? "/usr/local/OpenXM/doc/asir2000/html-jp/man_toc.html"))
+          (url-exists? "/usr/local/OpenXM/doc/asir2000/html-jp/man_toc.html"))
     |
     ((balloon (icon "tm_help.xpm") "Risa/Asir manual")
      (w3m-local "doc/asir2000/html-eg/man_toc.html"))))
   (menu-extend texmacs-extra-menu
     (if (in-openxm?)
       (=> "OpenXM"
-		("Asir mode"    (insert-string "!asir;"))
-		("Kan/sm1 mode" (insert-string "!sm1;"))
-		---
-		(-> "Manuals"
-			("Risa/Asir manual"    (w3m-local "doc/asir2000/html-jp/man_toc.html"))
-			("Asir-contrib manual" (w3m-local "doc/asir-contrib/html-ja/cman-ja_toc.html")))
-		(-> "Web"
-			("The OpenXM Project"  (w3m "http://www.math.kobe-u.ac.jp/OpenXM/")))
-		)))
+        (-> "Select engines"
+          ("Risa/Asir" (insert-string "!asir;"))
+          ("Kan/sm1"   (insert-string "!sm1;")))
+        (-> "Output format"
+          ("LaTeX"     (my-openxm-plugin-eval "!latex;"))
+          ("verbatim"  (my-openxm-plugin-eval "!verbatim;")))
+        ---
+        (-> "Manuals"
+          ("Risa/Asir manual"    (w3m-local "doc/asir2000/html-jp/man_toc.html"))
+          ("Asir-contrib manual" (w3m-local "doc/asir-contrib/html-ja/cman-ja_toc.html")))
+        (-> "Web"
+          ("The OpenXM Project"  (w3m "http://www.math.kobe-u.ac.jp/OpenXM/")))
+        )))
 )
 
 (plugin-configure openxm
