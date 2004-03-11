55502632457981a3dbb8f733cc60b2dd313419d7
ohara <>
2004-03-11 18:25:57 +0000

    Remove w3m-local and add w3m-search

--- src/kxx/init-openxm.scm
+++ src/kxx/init-openxm.scm
@@ -1,28 +1,34 @@
-; $OpenXM: OpenXM/src/kxx/init-openxm.scm,v 1.3 2004/03/10 17:31:13 ohara Exp $
+; $OpenXM: OpenXM/src/kxx/init-openxm.scm,v 1.4 2004/03/10 19:17:25 ohara Exp $
 
-(define (my-openxm-plugin-eval t)
+(define (openxm-plugin-eval t)
   (import-from (texmacs plugin plugin-cmd))
   (import-from (texmacs plugin plugin-convert))
   (plugin-eval "openxm" "default" (object->tree t)))
 
-(define (my-openxm-plugin-eval-paste t)
-  (insert-tree (object->tree (my-openxm-plugin-eval t))))
+(define (openxm-plugin-eval-paste t)
+  (insert-tree (object->tree (openxm-plugin-eval t))))
+
+(define (openxm-path t)
+  (if (string? t)
+    (let* ((openxm-home (or (getenv "OpenXM_HOME") "/usr/local/OpenXM"))
+		   (local-path (string-append openxm-home "/" t))
+		   (web-prefix "http://www.math.kobe-u.ac.jp/OpenXM/Current/")
+		   (web-path   (string-append web-prefix t)))
+      (if (url-exists? local-path) local-path web-path))))
 
 (define (w3m t)
   (if (string? t)
     (system (string-append "rxvt -g 100x50 -e w3m " t "&"))))
 
-(define (w3m-local t)
-  (if (string? t)
-    (w3m (string-append "/usr/local/OpenXM/" t))))
+(define (w3m-search t)
+  (w3m (openxm-path t)))
 
 (define (openxm-initialize)
   (menu-extend texmacs-session-help-icons
-    (if (and (in-openxm?)
-          (url-exists? "/usr/local/OpenXM/doc/asir2000/html-jp/man_toc.html"))
+    (if (in-openxm?)
     |
     ((balloon (icon "tm_help.xpm") "Risa/Asir manual")
-     (w3m-local "doc/asir2000/html-eg/man_toc.html"))))
+     (w3m-search "doc/asir2000/html-jp/man_toc.html"))))
   (menu-extend texmacs-extra-menu
     (if (in-openxm?)
       (=> "OpenXM"
@@ -30,12 +36,14 @@
           ("Risa/Asir" (insert-string "!asir;"))
           ("Kan/sm1"   (insert-string "!sm1;")))
         (-> "Output format"
-          ("LaTeX"     (my-openxm-plugin-eval "!latex;"))
-          ("verbatim"  (my-openxm-plugin-eval "!verbatim;")))
+          ("LaTeX"     (openxm-plugin-eval "!latex;"))
+          ("verbatim"  (openxm-plugin-eval "!verbatim;")))
         ---
         (-> "Manuals"
-          ("Risa/Asir manual"    (w3m-local "doc/asir2000/html-jp/man_toc.html"))
-          ("Asir-contrib manual" (w3m-local "doc/asir-contrib/html-ja/cman-ja_toc.html")))
+          ("Risa/Asir manual"
+           (w3m-search "doc/asir2000/html-jp/man_toc.html"))
+          ("Asir-contrib manual"
+           (w3m-search "doc/asir-contrib/html-ja/cman-ja_toc.html")))
         (-> "Web"
           ("The OpenXM Project"  (w3m "http://www.math.kobe-u.ac.jp/OpenXM/")))
         )))
@@ -45,4 +53,4 @@
   (:require (and (url-exists-in-path? "openxm") (url-exists-in-path? "ox_texmacs")))
   (:initialize (openxm-initialize))
   (:launch "exec openxm ox_texmacs")
-  (:session "OpenXM"))
+  (:session "OpenXM[asir,sm1]"))
