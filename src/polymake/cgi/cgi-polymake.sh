f6c8c319d5ce868aa41ff771744a78a662375069
takayama <>
2004-09-27 08:33:37 +0000

    A bug fix (TMP should be set to execute as NOBODY.)

--- src/polymake/cgi/cgi-polymake.sh
+++ src/polymake/cgi/cgi-polymake.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# $OpenXM$ 
+# $OpenXM: OpenXM/src/polymake/cgi/cgi-polymake.sh,v 1.1 2004/09/27 01:20:46 takayama Exp $ 
+PATH=/usr/local/polymake/bin:$PATH
+## Replace $OpenXM_HOME by an absolute path of your cite.
 source $(OpenXM_HOME)/rc/dot.bashrc
-sm1 -q -s "[(parse) (oxs.sm1) pushfile] extension oxNoX [(parse) (cgi.sm1) pushfile] extension cgiPolymake quit "
+export TMP=/tmp
+sm1 -q -s "[(ox.sm1) pushfile] extension oxNoX [(parse) (cgi.sm1) pushfile] extension cgiPolymake quit "
