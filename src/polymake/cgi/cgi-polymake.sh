a0e22ef9059629e70108d07f04b5c55c1e57773f
takayama <>
2004-11-23 04:34:31 +0000

    Fixed a bug (parse was missing.)

--- src/polymake/cgi/cgi-polymake.sh
+++ src/polymake/cgi/cgi-polymake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $OpenXM: OpenXM/src/polymake/cgi/cgi-polymake.sh,v 1.1 2004/09/27 01:20:46 takayama Exp $ 
+# $OpenXM: OpenXM/src/polymake/cgi/cgi-polymake.sh,v 1.2 2004/09/27 08:33:37 takayama Exp $ 
 PATH=/usr/local/polymake/bin:$PATH
 ## Replace $OpenXM_HOME by an absolute path of your cite.
 source $(OpenXM_HOME)/rc/dot.bashrc
 export TMP=/tmp
-sm1 -q -s "[(ox.sm1) pushfile] extension oxNoX [(parse) (cgi.sm1) pushfile] extension cgiPolymake quit "
+sm1 -q -s "[(parse) (ox.sm1) pushfile] extension oxNoX [(parse) (cgi.sm1) pushfile] extension cgiPolymake quit "
