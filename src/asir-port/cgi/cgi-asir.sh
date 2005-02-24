ffe9e16f410e2b3d84a0a0b287dde144daec2a37
takayama <>
2005-02-24 06:13:30 +0000

    Install kit of cgi-asir. Try also http://polymake.math.kobe-u.ac.jp/cgi-bin/cgi-asir.sh

---
+++ src/asir-port/cgi/cgi-asir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# $Id$
+# $OpenXM$
+## Replace $OpenXM_HOME by an absolute path of your cite.
+#source $(OpenXM_HOME)/rc/dot.bashrc
+source /home/taka/OpenXM/rc/dot.bashrc
+export TMP=/tmp
+export CGI_ASIR_ALLOW="[(version) (asir_contrib_copyright) (fctr) (primadec) (nd_gr_main) (dp_gr_main)]"
+export CGI_ASIR_TLIMIT="30"
+sm1 -q -s "[(parse) (cgiasir.sm1) pushfile] extension oxNoX cgiAsir quit "
