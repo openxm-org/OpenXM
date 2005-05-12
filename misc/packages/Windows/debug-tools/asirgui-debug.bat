07057f40fb5dd81d5c0d73578646edf08d37701c
takayama <>
2005-05-12 06:12:03 +0000

    Removed the first ';' in the ASIRLOADPATH.

--- misc/packages/Windows/debug-tools/asirgui-debug.bat
+++ misc/packages/Windows/debug-tools/asirgui-debug.bat
@@ -1,4 +1,4 @@
-rem $OpenXM: OpenXM/misc/packages/Windows/debug-tools/asirgui-debug.bat,v 1.1 2003/07/21 08:07:37 takayama Exp $
+rem $OpenXM: OpenXM/misc/packages/Windows/debug-tools/asirgui-debug.bat,v 1.2 2003/07/29 07:38:16 takayama Exp $
 rem
 rem
 set OpenXM_HOME=/cygdrive/c/cygwin/home/%USERNAME%/OpenXM/misc/packages/Windows/OpenXM-win
@@ -10,8 +10,9 @@ set OpenXM_START_EXE=start
 set WIN_ASIR_ROOT=%OpenXM_HOME_WIN%\asir
 set OpenXM_WIN_ROOT=%WIN_ASIR_ROOT%
 set PATH=%OpenXM_HOME_WIN%\bin;%PATH%
+rem ASIR_LIBDIR will be obsolete.
 set ASIR_LIBDIR=%OpenXM_HOME_WIN%\lib\asir;%WIN_ASIR_ROOT%\lib
-set ASIRLOADPATH=;%WIN_ASIR_ROOT%\lib;%OpenXM_HOME_WIN%\src\asir-contrib\packages\src;%OpenXM_HOME_WIN%\src\asir-contrib\packages\sample;%OpenXM_HOME_WIN%\lib\asir-contrib;.
+set ASIRLOADPATH=%WIN_ASIR_ROOT%\lib;%OpenXM_HOME_WIN%\src\asir-contrib\packages\src;%OpenXM_HOME_WIN%\src\asir-contrib\packages\sample;%OpenXM_HOME_WIN%\lib\asir-contrib;.
 set ASIR_CONFIG=%OpenXM_HOME_WIN%\rc\asirrc
 set ASIR_RSH='ssh -f -X -A '
 set LOAD_SM1_PATH=%OpenXM_HOME%/lib/sm1
