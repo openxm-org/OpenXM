15d5d8ad7ddd4eb4f1595008c95f7e1c54dd8263
takayama <>
2009-02-15 01:51:12 +0000

    The -split option of texi2html makes different behaviors between version 1.75 and 1.56. Asir contrib no longer uses the -split option.

--- src/asir-contrib/packages/doc/top/fix-link.sh
+++ src/asir-contrib/packages/doc/top/fix-link.sh
@@ -1,73 +1,78 @@
 #!/bin/sh
 # fix-link.sh ja ~/OpenXM/bin/oxreplace
-# $OpenXM: OpenXM/src/asir-contrib/packages/doc/top/fix-link.sh,v 1.3 2009/02/12 08:12:58 takayama Exp $
+# $OpenXM: OpenXM/src/asir-contrib/packages/doc/top/fix-link.sh,v 1.4 2009/02/14 08:34:38 takayama Exp $
 LLL=$1
 OXREPLACE=$2
 
 NEW0="[[dsolv]]"
-NEW1="dsolv-html/dsolv-${LLL}/dsolv-${LLL}_toc.html"
+NEW1="dsolv-html/dsolv-${LLL}.html"
 NEW2="dsolv (Solving the initial ideal for holonomic systems)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[ok_diff]]"
-NEW1="ok_diff-html/ok_diff-${LLL}/ok_diff-${LLL}_toc.html"
+NEW1="ok_diff-html/ok_diff-${LLL}.html"
 NEW2="ok_diff (Okutani library for differential operators)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[ok_dmodule]]"
-NEW1="ok_dmodule-html/ok_dmodule-${LLL}/ok_dmodule-${LLL}_toc.html"
+NEW1="ok_dmodule-html/ok_dmodule-${LLL}.html"
 NEW2="ok_dmodule (Okutani library for D-modules)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[plucker]]"
-NEW1="plucker-html/plucker-${LLL}/plucker-${LLL}_toc.html"
+NEW1="plucker-html/plucker-${LLL}.html"
 NEW2="plucker (Plucker relations)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[pfpcoh]]"
-NEW1="pfpcoh-html/pfpcoh-${LLL}/pfpcoh-${LLL}_toc.html"
+NEW1="pfpcoh-html/pfpcoh-${LLL}.html"
 NEW2="pfpcoh (Ohara library for homology/cohomology groups for p F q )"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[gnuplot]]"
-NEW1="gnuplot-html/gnuplot-${LLL}/gnuplot-${LLL}_toc.html"
+NEW1="gnuplot-html/gnuplot-${LLL}.html"
 NEW2="gnuplot (gnuplot ox server for graphics)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[mathematica]]"
-NEW1="mathematica-html/mathematica-${LLL}/mathematica-${LLL}_toc.html"
+NEW1="mathematica-html/mathematica-${LLL}.html"
 NEW2="mathematica (Mathematica (TM) ox server)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[om]]"
-NEW1="om-html/om-${LLL}/om-${LLL}_toc.html"
+NEW1="om-html/om-${LLL}.html"
 NEW2="om (om (java) ox server for translating CMO and OpenMath)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[phc]]"
-NEW1="phc-html/phc-${LLL}/phc-${LLL}_toc.html"
+NEW1="phc-html/phc-${LLL}/phc-${LLL}.html"
 NEW2="phc (PHC ox server for solving systems of algebraic equations by the homotopy method)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[sm1]]"
-NEW1="sm1-html/sm1-${LLL}/sm1-${LLL}_toc.html"
+NEW1="sm1-html/sm1-${LLL}.html"
 NEW2="sm1 (Kan/sm1 ox server for the ring of differential operators)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[tigers]]"
-NEW1="tigers-html/tigers-${LLL}/tigers-${LLL}_toc.html"
+NEW1="tigers-html/tigers-${LLL}.html"
 NEW2="tigers (tigers ox server for toric universal Grobner bases)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[todo_parametrize]]"
-NEW1="todo_parametrize-html/todo_parametrize-${LLL}/todo_parametrize-${LLL}_toc.html"
+NEW1="todo_parametrize-html/todo_parametrize-${LLL}.html"
 NEW2="todo_parametrize (Todo library for parametrizing algebraic curves)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
+
+NEW0="[[f_res]]"
+NEW1="f_res-html/f_res-${LLL}.html"
+NEW2="f_res (computing resultants )"
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
 NEW0="[[mt_graph]]"
-NEW1="mk_graph-html/mk_graph-${LLL}/mk_graph-${LLL}_toc.html"
+NEW1="mk_graph-html/mk_graph-${LLL}.html"
 NEW2="mt_graph (mt_graph 3D grapher)"
-${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../../${NEW1}\"> ${NEW2} </a>" cman-${LLL}_toc.html
+${OXREPLACE} --replaceLine --old "${NEW0}" --new "<li> <a href=\"../${NEW1}\"> ${NEW2} </a>" cman-${LLL}.html
 
-rm -f cman-ja_toc*.old
+rm -f cman-*.old
 
