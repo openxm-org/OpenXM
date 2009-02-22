8e4b38eb0a4f49c49dafb0cb8801e52f3dfc6b5a
takayama <>
2009-02-22 08:16:44 +0000

    Preliminary version of oxhtmlhelp, which shows help files.

---
+++ src/util/oxhtmlhelp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFSIZE 1024
+/* BUG:  it is only for knoppix/math */
+
+main(int argc, char *argv[]) {
+  char *oxhome;
+  char browser[BUFSIZE];
+  char fname[BUFSIZE];
+  char comm[BUFSIZE];
+  int i;
+
+  fname[0] = 0;
+  oxhome = getenv("OpenXM_HOME");
+  if (oxhome == NULL) oxhome="/usr/local/OpenXM";
+  for (i=1; i<argc; i++) {
+    strcpy(fname,argv[i]);
+  }
+  if (getenv("BROWSER") == NULL) {
+    strcpy(browser,"firefox");
+  }else{
+    strcpy(browser,getenv("BROWSER"));
+  }
+
+  if (fname[0] == 0) {
+    strcpy(fname,oxhome);
+    strcat(fname,"/doc");
+  }
+
+  sprintf(comm,"%s %s",browser,fname);
+  system(comm);
+
+  return 0;
+
+}
