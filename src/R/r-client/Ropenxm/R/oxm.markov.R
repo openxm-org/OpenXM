5da580cc304af4cbe86db50f5bcf0a59b7a6f8a9
takayama <>
2013-02-02 01:33:30 +0000

    source("oxm.markov.R"); a<-oxm.markov(matrix(c(1,2,3),nrow=1,ncol=3)); returns the Markov basis for the set of points [[1,2,3]]. Computation is done by 4ti2 through cgi-asir->nk_4ti2. a$markov[[1]], a$markov[[2]] are the basis in this example.

--- src/R/r-client/Ropenxm/R/oxm.markov.R
+++ src/R/r-client/Ropenxm/R/oxm.markov.R
@@ -1,3 +1,23 @@
 oxm.markov <-
-function(mat) { return(mat); }
+function(mat=matrix(c(1,2,3),nrow=1,ncol=3),
+   url="http://polymake.math.kobe-u.ac.jp/cgi-bin/cgi-asir-r-markov.sh") { 
+  cmd<-oxm.matrix_r2asir(mat,s="r_markov([",s.end="])");
+  ans<-postForm(url,oxMessageBody=cmd,style="POST");
+  mar<-eval(parse(text=ans));
+  return(list(markov=mar,text=ans));
+}
 
+oxm.matrix_r2asir<-
+function(mat=matrix(c(1,2,3,4),nrow=2,ncol=2),s="[",s.end="]") {
+  m<-dim(mat)[1];n<-dim(mat)[2];
+  for (i in seq(1:m)) {
+   s <- paste(s,"[");
+   for (j in seq(1,n)) {
+     if (j != n) s<-paste(s,mat[i,j],",");
+   }
+   s<-paste(s,mat[i,n],"]");
+   if (i != m) s<-paste(s,",");
+  }
+  s<-paste(s,s.end);
+  return(s);
+}
