ed049c9eaf92b4b892c297700915742ed3877b40
takayama <>
2013-02-06 07:39:32 +0000

    Added an easy check if the returned value is not illegal. In the previous implementation, if a fake server sends a return value like system("...."), the local system executes it and it can be a serious security hole.

--- src/R/r-client/Ropenxm/R/oxm.markov.R
+++ src/R/r-client/Ropenxm/R/oxm.markov.R
@@ -1,11 +1,19 @@
-#$OpenXM: OpenXM/src/R/r-client/Ropenxm/R/oxm.markov.R,v 1.3 2013/02/02 01:34:41 takayama Exp $
+#$OpenXM: OpenXM/src/R/r-client/Ropenxm/R/oxm.markov.R,v 1.4 2013/02/02 05:16:44 takayama Exp $
 oxm.markov <-
 function(mat=matrix(c(1,2,3),nrow=1,ncol=3),
    url="http://polymake.math.kobe-u.ac.jp/cgi-bin/cgi-asir-r-markov.sh") { 
   cmd<-oxm.matrix_r2tfb(mat,s="r_markov([",s.end="])");
   ans<-postForm(url,oxMessageBody=cmd,style="POST");
   pos<-pmatch("list(",c(ans),nomatch=0);
-  if (pos !=0) {mar<-eval(parse(text=ans));
+
+  # security check against that a fake server sends an illegal R command.
+  len<-length(grep("[A-Zab]",ans));
+  len<-len+length(grep("[d-h]",ans));
+  len<-len+length(grep("[jk]",ans));
+  len<-len+length(grep("[m-r]",ans));
+  len<-len+length(grep("[u-z]",ans));
+
+  if ((pos !=0) && (len == 0)) {mar<-eval(parse(text=ans));
   }else {mar<-NA;}
   return(list(markov=mar,text=ans));
 }
