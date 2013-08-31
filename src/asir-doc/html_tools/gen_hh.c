9dbf6bef2a0ce34018b3a8f62317d52e07b52d4a
ohara <>
2013-08-31 13:45:59 +0000

    gen_hh.c and gen_hh_new.c can be built by MS Visual Studio.

--- src/asir-doc/html_tools/gen_hh.c
+++ src/asir-doc/html_tools/gen_hh.c
@@ -1,13 +1,15 @@
-/* $OpenXM$ */
+/* $OpenXM: OpenXM/src/asir-doc/html_tools/gen_hh.c,v 1.2 2009/02/22 22:48:15 ohara Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#if !defined(_MSC_VER)
 #include <dirent.h>
+#else 
+#include <windows.h>
+#endif
 
-gen_hhp(out,n)
-char *out;
-int n;
+void gen_hhp(char *out, int n, char *htmldir)
 {
 	int i;
 	FILE *outf;
@@ -17,20 +19,19 @@ int n;
 	fprintf(outf,"Compatibility=1.1 or later\n");
 	fprintf(outf,"Compiled file=asirhelp.chm\n");
 	fprintf(outf,"Contents file=asirhelp.hhc\n");
-	fprintf(outf,"Default topic=html\\man_toc.html\n");
+	fprintf(outf,"Default topic=%s\\man_toc.html\n",htmldir);
 	fprintf(outf,"Display compile progress=No\n");
 	fprintf(outf,"Index file=asirhelp.hhk\n");
 	fprintf(outf,"Language=0x411 “ú–{Œê\n\n\n[FILES]\n");
 
-	fprintf(outf,"html\\man_toc.html\n");
+	fprintf(outf,"%s\\man_toc.html\n",htmldir);
 	for ( i = 1; i <= n; i++ )
-		fprintf(outf,"html\\man_%d.html\n",i);
+		fprintf(outf,"%s\\man_%d.html\n",htmldir,i);
 
 	fprintf(outf,"\n[INFOTYPES]\n");
 }
 
-conv_toc(in,out)
-char *in,*out;
+void conv_toc(char *in,char *out, char *htmldir)
 {
 	char buf[BUFSIZ];
 	char *fname,*ptr,*ptr1;
@@ -66,15 +67,14 @@ char *in,*out;
 			*ptr1 = 0;
 			fprintf(outf,"<LI><OBJECT type=\"text/sitemap\">\n");
 			fprintf(outf,"<param name=\"Name\" value=\"%s\">\n",ptr);
-			fprintf(outf,"<param name=\"Local\" value=\"html\\%s\">\n",fname);
+			fprintf(outf,"<param name=\"Local\" value=\"%s\\%s\">\n",htmldir,fname);
 			fprintf(outf,"</OBJECT>\n");
 		}
 	}
 	fprintf(outf,"</UL>\n</BODY></HTML>\n");
 }
 
-conv_index (in,out)
-char *in,*out;
+void conv_index(char *in, char *out, char *htmldir)
 {
 	char buf[BUFSIZ];
 	char *fname,*ptr,*ptr1;
@@ -109,34 +109,30 @@ char *in,*out;
 			*ptr1 = 0;
 			fprintf(outf,"<LI><OBJECT type=\"text/sitemap\">\n");
 			fprintf(outf,"<param name=\"Name\" value=\"%s\">\n",ptr);
-			fprintf(outf,"<param name=\"Local\" value=\"html\\%s\">\n",fname);
+			fprintf(outf,"<param name=\"Local\" value=\"%s\\%s\">\n",htmldir,fname);
 			fprintf(outf,"</OBJECT>\n");
 		}
 	}
 	fprintf(outf,"</BODY></HTML>\n");
 }
 
-main(argc,argv)
-int argc;
-char **argv;
+#if !defined(_MSC_VER)
+int find_files(char *indir,char *prefix) 
 {
 	DIR *d;
 	struct dirent *dent;
-	int i,n,n1;
+	int n=0,n1;
 	char *ptr,*ptr1;
-	char *indir,*outdir;
-	char in[BUFSIZ],out[BUFSIZ],name[BUFSIZ];
-
-	indir = argv[1];
-	outdir = argv[2];
-	sprintf(in,"%s/man_toc.html",indir);
-	sprintf(out,"%s/asirhelp.hhc",outdir);
-	conv_toc(in,out);
+	char name[BUFSIZ];
+	int len=strlen(prefix);
 	d = opendir(indir);
-	n = 0;
+	if(!d) {
+		exit(1);
+	}
 	while ( dent = readdir(d) ) {
 		strcpy(name,dent->d_name);
-		ptr = strchr(name,'_');
+		ptr = name+len;
+		ptr = strchr(ptr,'_');
 		if ( !ptr )
 			continue;
 		ptr++;
@@ -150,10 +146,62 @@ char **argv;
 		if ( n1 > n )
 			n = n1;
 	}
+	closedir(d);
+	return n;
+}
+#else
+int find_files(char *indir,char *prefix) 
+{
+    HANDLE h;
+    WIN32_FIND_DATA fd;
+    char *ptr,*ptr1;
+    char pattern[BUFSIZ];
+    char name[BUFSIZ];
+	int n=0,n1;
+    int len=strlen(prefix);
+    sprintf(pattern, "%s\\%s*_*.*", indir, prefix);
+    h = FindFirstFileEx(pattern, FindExInfoStandard, &fd, FindExSearchNameMatch, NULL, 0);
+    if(h == INVALID_HANDLE_VALUE) {
+        exit(1);
+    }
+    do {
+        strcpy(name,fd.cFileName);
+        ptr = name+len;
+        ptr = strchr(ptr,'_') + 1;
+        ptr1 = strchr(ptr,'.');
+        *ptr1 = 0;
+        if ( !strcmp(ptr,"toc") )
+            continue;
+        n1 = atoi(ptr);
+        if ( n1 > n )
+            n = n1;
+    } while(FindNextFile(h, &fd));
+    return n;
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+	int n;
+	char *indir,*outdir;
+	char in[BUFSIZ],out[BUFSIZ];
+	char *htmldir="html";
+
+	indir = argv[1];
+	outdir = argv[2];
+	if(argc>3) {
+		htmldir=argv[3];
+	}
+
+	sprintf(in,"%s/man_toc.html",indir);
+	sprintf(out,"%s/asirhelp.hhc",outdir);
+	conv_toc(in,out,htmldir);
+	n = find_files(indir, "");
+
 	sprintf(in,"%s/man_%d.html",indir,n);
 	sprintf(out,"%s/asirhelp.hhk",outdir);
-	conv_index(in,out);
+	conv_index(in,out,htmldir);
 	sprintf(out,"%s/asirhelp.hhp",outdir);
-	gen_hhp(out,n);
+	gen_hhp(out,n,htmldir);
 	exit(0);
 }
--- src/asir-doc/html_tools/gen_hh_new.c
+++ src/asir-doc/html_tools/gen_hh_new.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#if !defined(_MSC_VER)
 #include <dirent.h>
+#else 
+#include <windows.h>
+#endif
 
-gen_hhp(out,n,prefix,lang)
-char *out;
-int n;
-char *prefix,*lang;
+void gen_hhp(char *out, int n, char *prefix, char *lang, char *htmldir)
 {
 	int i;
 	FILE *outf;
@@ -15,20 +17,19 @@ char *prefix,*lang;
 	fprintf(outf,"Compatibility=1.1 or later\n");
 	fprintf(outf,"Compiled file=%shelp-%s.chm\n",prefix,lang);
 	fprintf(outf,"Contents file=%shelp-%s.hhc\n",prefix,lang);
-	fprintf(outf,"Default topic=html\\%s-%s_toc.html\n",prefix,lang);
+	fprintf(outf,"Default topic=%s\\%s-%s_toc.html\n",htmldir,prefix,lang);
 	fprintf(outf,"Display compile progress=No\n");
 	fprintf(outf,"Index file=%shelp-%s.hhk\n",prefix,lang);
 	fprintf(outf,"Language=0x411 “ú–{Œê\n\n\n[FILES]\n");
 
-	fprintf(outf,"html\\%s-%s_toc.html\n",prefix,lang);
+	fprintf(outf,"%s\\%s-%s_toc.html\n",htmldir,prefix,lang);
 	for ( i = 1; i <= n; i++ )
-		fprintf(outf,"html\\%s-%s_%d.html\n",prefix,lang,i);
+		fprintf(outf,"%s\\%s-%s_%d.html\n",htmldir,prefix,lang,i);
 
 	fprintf(outf,"\n[INFOTYPES]\n");
 }
 
-conv_toc(in,out,prefix_)
-char *in,*out,*prefix_;
+void conv_toc(char *in, char *out, char *prefix_, char *htmldir)
 {
 	char buf[BUFSIZ];
 	char *fname,*ptr,*ptr1;
@@ -64,15 +65,14 @@ char *in,*out,*prefix_;
 			*ptr1 = 0;
 			fprintf(outf,"<LI><OBJECT type=\"text/sitemap\">\n");
 			fprintf(outf,"<param name=\"Name\" value=\"%s\">\n",ptr);
-			fprintf(outf,"<param name=\"Local\" value=\"html\\%s\">\n",fname);
+			fprintf(outf,"<param name=\"Local\" value=\"%s\\%s\">\n",htmldir,fname);
 			fprintf(outf,"</OBJECT>\n");
 		}
 	}
 	fprintf(outf,"</UL>\n</BODY></HTML>\n");
 }
 
-conv_index (in,out,prefix_)
-char *in,*out,*prefix_;
+void conv_index (char *in, char *out, char *prefix_, char *htmldir)
 {
 	char buf[BUFSIZ];
 	char *fname,*ptr,*ptr1;
@@ -107,36 +107,26 @@ char *in,*out,*prefix_;
 			*ptr1 = 0;
 			fprintf(outf,"<LI><OBJECT type=\"text/sitemap\">\n");
 			fprintf(outf,"<param name=\"Name\" value=\"%s\">\n",ptr);
-			fprintf(outf,"<param name=\"Local\" value=\"html\\%s\">\n",fname);
+			fprintf(outf,"<param name=\"Local\" value=\"%s\\%s\">\n",htmldir,fname);
 			fprintf(outf,"</OBJECT>\n");
 		}
 	}
 	fprintf(outf,"</BODY></HTML>\n");
 }
 
-main(argc,argv)
-int argc;
-char **argv;
+#if !defined(_MSC_VER)
+int find_files(char *indir,char *prefix) 
 {
 	DIR *d;
 	struct dirent *dent;
-	int i,n,n1,len;
+	int n=0,n1;
 	char *ptr,*ptr1;
-	char *indir,*outdir;
-	char in[BUFSIZ],out[BUFSIZ],name[BUFSIZ],prefix_[BUFSIZ];
-	char *prefix,*lang;
-
-	indir = argv[1];
-	outdir = argv[2];
-	prefix = argv[3];
-	lang = argv[4];
-	len = strlen(prefix);
-	sprintf(in,"%s/%s-%s_toc.html",indir,prefix,lang);
-	sprintf(out,"%s/%shelp-%s.hhc",outdir,prefix,lang);
-	sprintf(prefix_,"%s-%s_",prefix,lang);
-	conv_toc(in,out,prefix_);
+	char name[BUFSIZ];
+	int len=strlen(prefix);
 	d = opendir(indir);
-	n = 0;
+	if(!d) {
+		exit(1);
+	}
 	while ( dent = readdir(d) ) {
 		strcpy(name,dent->d_name);
 		ptr = name+len;
@@ -154,10 +144,64 @@ char **argv;
 		if ( n1 > n )
 			n = n1;
 	}
+	closedir(d);
+	return n;
+}
+#else
+int find_files(char *indir,char *prefix) 
+{
+    HANDLE h;
+    WIN32_FIND_DATA fd;
+    char *ptr,*ptr1;
+    char pattern[BUFSIZ];
+    char name[BUFSIZ];
+	int n=0,n1;
+    int len=strlen(prefix);
+    sprintf(pattern, "%s\\%s*_*.*", indir, prefix);
+    h = FindFirstFileEx(pattern, FindExInfoStandard, &fd, FindExSearchNameMatch, NULL, 0);
+    if(h == INVALID_HANDLE_VALUE) {
+        exit(1);
+    }
+    do {
+        strcpy(name,fd.cFileName);
+        ptr = name+len;
+        ptr = strchr(ptr,'_') + 1;
+        ptr1 = strchr(ptr,'.');
+        *ptr1 = 0;
+        if ( !strcmp(ptr,"toc") )
+            continue;
+        n1 = atoi(ptr);
+        if ( n1 > n )
+            n = n1;
+    } while(FindNextFile(h, &fd));
+    return n;
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+	int n;
+	char *indir,*outdir;
+	char in[BUFSIZ],out[BUFSIZ],prefix_[BUFSIZ];
+	char *prefix,*lang;
+	char *htmldir = "html";
+
+	indir = argv[1];
+	outdir = argv[2];
+	prefix = argv[3];
+	lang = argv[4];
+	if(argc>5) {
+		htmldir = argv[5];
+	}
+	sprintf(in,"%s/%s-%s_toc.html",indir,prefix,lang);
+	sprintf(out,"%s/%shelp-%s.hhc",outdir,prefix,lang);
+	sprintf(prefix_,"%s-%s_",prefix,lang);
+	conv_toc(in,out,prefix_,htmldir);
+	n = find_files(indir,prefix);
 	sprintf(in,"%s/%s-%s_%d.html",indir,prefix,lang,n);
 	sprintf(out,"%s/%shelp-%s.hhk",outdir,prefix,lang);
-	conv_index(in,out,prefix_);
+	conv_index(in,out,prefix_,htmldir);
 	sprintf(out,"%s/%shelp-%s.hhp",outdir,prefix,lang);
-	gen_hhp(out,n,prefix,lang);
-	exit(0);
+	gen_hhp(out,n,prefix,lang,htmldir);
+	return 0;
 }
