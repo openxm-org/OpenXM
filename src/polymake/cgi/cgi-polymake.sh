5ad0c6cc63ab64a17484ca0cf6372a68a1d5391e
takayama <>
2013-09-22 02:28:42 +0000

    Use oxdecode_post.

--- src/polymake/cgi/cgi-polymake.sh
+++ src/polymake/cgi/cgi-polymake.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# $OpenXM: OpenXM/src/polymake/cgi/cgi-polymake.sh,v 1.2 2004/09/27 08:33:37 takayama Exp $ 
+# $OpenXM: OpenXM/src/polymake/cgi/cgi-polymake.sh,v 1.3 2004/11/23 04:34:31 takayama Exp $ 
 PATH=/usr/local/polymake/bin:$PATH
-## Replace $OpenXM_HOME by an absolute path of your cite.
-source $(OpenXM_HOME)/rc/dot.bashrc
 export TMP=/tmp
-sm1 -q -s "[(parse) (ox.sm1) pushfile] extension oxNoX [(parse) (cgi.sm1) pushfile] extension cgiPolymake quit "
+# Set OpenXM_HOME with your absolute path
+# Ex. OpenXM_HOME=/home/taka/OpenXM
+source $(OpenXM_HOME)/rc/dot.bashrc
+WFILE=/tmp/tmp-post-$$.txt
+oxdecode_post >${WFILE}
+env CONTENT_LENGTH=`wc -m ${WFILE} | awk '{print $1}'` sm1 -q -s "[(parse) (cgi.sm1) pushfile] extension cgiPolymake quit " <${WFILE}
+rm -f ${WFILE}
+exit 0
+: rm -f ${WFILE}
