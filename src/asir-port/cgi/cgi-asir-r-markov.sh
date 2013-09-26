7c79d6799fd84759906bb605187d29efc318f524
takayama <>
2013-09-26 04:13:40 +0000

    cgi-asir*.sh supports post messages encoded in mime.

--- src/asir-port/cgi/cgi-asir-r-markov.sh
+++ src/asir-port/cgi/cgi-asir-r-markov.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $OpenXM: OpenXM/src/asir-port/cgi/cgi-asir-r-markov.sh,v 1.1 2013/01/31 06:50:08 takayama Exp $
+# $OpenXM: OpenXM/src/asir-port/cgi/cgi-asir-r-markov.sh,v 1.2 2013/02/18 23:33:13 takayama Exp $
 ## Replace $OpenXM_HOME by an absolute path of your cite.
 source $OpenXM_HOME/rc/dot.bashrc
 #source /home/taka/OpenXM/rc/dot.bashrc
@@ -10,5 +10,9 @@ export OX_ASIR_LOAD_FILE=$OpenXM_HOME/src/asir-port/cgi/r-markov.rr
 export CGI_ASIR_MSG="<pre>
  Example. r_markov([[1,1,1,1],[0,1,2,3]]) 
 </pre><br>"
-sm1 -q -s "[(parse) (cgiasir.sm1) pushfile] extension oxNoX cgiAsir quit "
+WFILE=/tmp/tmp-post-$$.txt
+tee /tmp/t.txt | oxdecode_post >${WFILE}
+env CONTENT_LENGTH=`wc -m ${WFILE} | awk '{print $1}'`  sm1 -q -s "[(parse) (cgiasir.sm1) pushfile] extension oxNoX cgiAsir quit " <${WFILE}
+rm -f ${WFILE}
+exit 0
 
--- src/asir-port/cgi/cgi-asir.sh
+++ src/asir-port/cgi/cgi-asir.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # $Id$
-# $OpenXM: OpenXM/src/asir-port/cgi/cgi-asir.sh,v 1.3 2005/02/27 07:08:11 takayama Exp $
+# $OpenXM: OpenXM/src/asir-port/cgi/cgi-asir.sh,v 1.4 2005/02/27 08:22:56 takayama Exp $
 ## Replace $OpenXM_HOME by an absolute path of your cite.
 #source $(OpenXM_HOME)/rc/dot.bashrc
 source /home/taka/OpenXM/rc/dot.bashrc
 export TMP=/tmp
 export CGI_ASIR_ALLOW="[(version) (asir_contrib_copyright) (fctr) (primadec) (nd_gr_main) (dp_gr_main)]"
 export CGI_ASIR_TLIMIT="30"
-sm1 -q -s "[(parse) (cgiasir.sm1) pushfile] extension oxNoX cgiAsir quit "
+WFILE=/tmp/tmp-post-$$.txt
+tee /tmp/t.txt | oxdecode_post >${WFILE}
+env CONTENT_LENGTH=`wc -m ${WFILE} | awk '{print $1}'`  sm1 -q -s "[(parse) (cgiasir.sm1) pushfile] extension oxNoX cgiAsir quit " <${WFILE}
+rm -f ${WFILE}
+exit 0
+
