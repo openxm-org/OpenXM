5fc1e585b3dfe74ce46a94a83ca25419da4ca275
takayama <>
2014-03-24 02:34:25 +0000

    The orthant module is linked with Makevars. hgm.ncorthant<-hgm.ko.ncorthant is defined in zhgm_names.R

--- src/R/r-packages/hgm/R/ko_orthant.R
+++ src/R/r-packages/hgm/R/ko_orthant.R
@@ -1,4 +1,5 @@
-dyn.load("hgm_ko_orthant.so")
+#dyn.load("hgm_ko_orthant.so")
+if (!is.loaded("hgm")) library.dynam("hgm",package="hgm",lib.loc=NULL);
 
 hgm.ko.ncorthant <- function(x,y,rk_step_size=1e-3)
       .C("hgm_ko_orthant_R",
---
+++ src/R/r-packages/hgm/R/zhgm_names.R
@@ -0,0 +1,6 @@
+hgm.so3nc<-hgm.tk.so3nc
+hgm.ncso3<-hgm.tk.so3nc
+hgm.normalizingConstantOfFisherDistributionOnSO3<-hgm.tk.so3nc
+hgm.pwishart<-hgm.tk.pwishart
+hgm.ncorthant<-hgm.ko.ncorthant
+hgm.normalizingConstantOfOrthant<-hgm.ko.ncorthant
