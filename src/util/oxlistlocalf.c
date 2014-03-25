71d84950cb20f8b43dc21cc4d62dd7e193a3ff4f
takayama <>
2014-03-25 07:08:25 +0000

    oxlistlocalf asirProgram outputs a list of the functions defined in the asirProgram. For example, when the asirProgram is as   def foo(X) {return(X);}   def afo(X,Y) { return(X+Y);} the oxlistlocalf outputs localf foo$ localf afo$
    I hope that it is useful to modulize asir programs.

---
+++ src/util/oxlistlocalf.c
@@ -0,0 +1,560 @@
+/* $OpenXM$ */
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "oxlistlocalf.h"
+
+objectp KClval;
+int Replace = 0;
+int Linenumber = 0;
+int Saki = 0;
+int Debug2 = 0;
+FILE *Inop = NULL;
+
+char *readstring(void);
+
+void *mymalloc(int n) { return malloc(n); }
+
+objectp newObject_d() {
+  objectp obj;
+  obj = (objectp) mymalloc(sizeof(struct Object));
+  if (obj == (objectp) NULL) {
+    fprintf(stderr,"Error: No more memory in newObject_d().\n");
+    exit(10);
+  }
+  obj->tag = Snull;  obj->attr = NULL;
+  return(obj);
+}
+
+char *newString(int size) {
+  char *str;
+  if (size <= 0) size=1;
+  str = (char *)mymalloc(sizeof(char)*size);
+  if (str == (char *)NULL) {
+    fprintf(stderr,"Error: No more memory in newObject_d().\n");
+    exit(10);
+  }
+  return(str);
+}
+
+void printObject_d(FILE *fp,objectp op)
+{
+  if (op == (objectp) NULL) {
+    fprintf(fp," <null objectp> ");
+    return;
+  }
+  switch(op->tag) {
+  case Sinteger:
+    fprintf(fp,"%d",op->lc.ival);
+    return;
+    break;
+  case Sstring:
+    fprintf(fp,"%s",op->lc.str);
+    return;
+    break;
+  default:
+    fprintf(stderr,"printObject_d(): Sorry. Not implemented.");
+    break;
+  }
+}
+
+void printObjectSymbol(objectp op)
+{
+  static char tmp[1024];
+  if (op == (objectp) NULL) {
+    printf(" <null objectp> ");
+    return;
+  }
+  switch(op->tag) {
+  case Sinteger:
+    printf("%d",op->lc.ival);
+    return; break;
+  case Sstring:
+    printf("%s",op->lc.str);
+    return; break;
+  default:
+    fprintf(stderr,"(printObjectSymbol(): Sorry. Not implemented.)");
+    break;
+  }
+}
+
+int KClex() {
+  extern int Saki; /* Saki-yomi */
+  int d;
+  int state = 0;
+  int i;
+  static char nametmp[1024];
+  char *name = nametmp;
+  char *str;
+  KClval = (objectp) NULL;
+  while (1) {
+    switch( state ) {
+    case 0:
+      /* printf(" <%x> ",Saki); */
+      if (Saki == EOF) {
+        return(Saki);
+      }
+      if (Saki <= ' ') {
+        if (Saki == '\n') ++Linenumber;
+        if (Replace) putchar0(Saki); 
+        Saki = fsgetc(Inop); break;
+      }
+      if ( Saki == '\"' ) {
+        str = readstring();
+        /* if (Mydebug) printf("[string: %s]",str); */
+
+        KClval = newObject_d();
+        KClval->tag = Sstring;
+        KClval->lc.str = newString(strlen(str)+1);
+        strcpy(KClval->lc.str,str);
+    
+        Saki = fsgetc(Inop);
+        return(QUOTE);
+        break;
+      }
+      if (Saki == '\'' ) {
+        d = readchar();
+        Saki = fsgetc(Inop);
+
+        KClval = newObject_d();
+        KClval->tag = Sinteger;
+        KClval->lc.ival = d;
+
+        return(SINGLEQUOTE);
+      }
+      /* single */
+      if ( Saki == '(' || Saki == ')' || Saki == ';' ||
+           Saki == '{' || Saki == '}' || Saki == ',' ||
+           Saki == '[' || Saki == ']' || Saki == '~' ||
+            Saki == '?' || Saki == '.' || Saki == '$') {
+        d = Saki; Saki = fsgetc(Inop);
+        if (Replace) putchar0(d);
+        return(d);
+      }
+
+      /* single or combination =, == */
+      if ( Saki == '=') {
+        state = 51; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '<' ) {
+        state = 52; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '>' ) {
+        state = 53; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '/' ) {
+        state = 54; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '&' ) {
+        state = 55; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '|' ) {
+        state = 56; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '!' ) {
+        state = 57; Saki = fsgetc(Inop); break;
+      } else if ( Saki == ':' ) {
+        state = 58; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '+' ) {
+        state = 59; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '-' ) {
+        state = 60; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '%' ) {
+        state = 61; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '^' ) {
+        state = 62; Saki = fsgetc(Inop); break;
+      } else if ( Saki == '*' ) {
+        state = 63; Saki = fsgetc(Inop); break;
+      } else ;
+
+
+      /* else : Identifier or function names. */
+      name[0] = Saki; i=1; name[i] = '\0';
+      Saki = fsgetc(Inop);
+      if (isdigit(name[0])) {
+        /*while (isdigit(Saki) || isalpha(Saki) || Saki=='.') */
+        while (isdigit(Saki) || isalpha(Saki)) {
+          name[i++] = Saki; name[i] = '\0';
+          Saki = fsgetc(Inop);
+        }
+      }else{
+        while (isdigit(Saki) || isalpha(Saki) || (Saki=='_') ||
+               ( Saki>= 256)) {
+          name[i++] = Saki; name[i] = '\0';
+          Saki = fsgetc(Inop);
+        }
+      }
+      /*if (Mydebug) printf("identifier string=[%s]",name);*/
+      if (isdigit(name[0])) {
+        /****************************
+      /**case : machine integer. 
+    KClval = newObject_d();
+    KClval->tag = Sinteger;
+    sscanf(name,"%d",&(KClval->lc.ival));*************/
+        /* Other cases.  */
+        KClval = newObject_d();
+        KClval->tag = Sstring;
+        KClval->lc.str = newString(strlen(name)+1);
+        strcpy(KClval->lc.str,name);
+        return(NUMBER);
+        break;
+      } /* else : Identifier case.*/
+      
+      if (d = isReserved(name)) {
+        if (Replace) printf0(name);
+        return(d);
+      } else {
+        if (Replace) {
+          if (shouldReplace(name))
+            printf1(name); /* do your own replacement in printf1*/
+          else
+            printf0(name);
+        }
+        KClval = newObject_d();
+        KClval->tag = Sstring;
+        KClval->lc.str = newString(strlen(name)+1);
+        strcpy(KClval->lc.str,name);
+        return(ID);
+      }
+      break;
+
+    case 51:
+      if (Replace) putchar0('=');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(EQUAL); /* == */
+      }else{
+        state = 0;return('=');
+      }
+      break;
+    case 52: /* 52 --- 60 tmporary return values */
+      if (Replace) putchar0('<');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(LESSEQUAL); /* <= */
+      } else if ( Saki == '<') {
+        if (Replace) putchar0('<');
+        Saki = fsgetc(Inop);state = 0;return(LEFTSHIFT); /* << */
+      }else{
+        state = 0;return('<');
+      }
+      break;
+    case 53: 
+      if (Replace) putchar0('>');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(GREATEREQUAL); /* >= */
+      } else if ( Saki == '>') {
+        if (Replace) putchar0('>');
+        Saki = fsgetc(Inop);state = 0;return(RIGHTSHIFT); /* >> */
+      }else{
+        state = 0;return('>');
+      }
+      break;
+    case 54: 
+      if ( Saki == '*') {
+        readcomment();
+        Saki = fsgetc(Inop);state = 0;break; /* clike-comment */
+      }else if (Saki == '/') {
+        readcomment2();
+        Saki = fsgetc(Inop);state = 0;break; /* comment by // */
+      }else {
+        if (Replace) putchar0('/');
+        state = 0;return('/');
+      }
+      break;
+    case 55: 
+      if (Replace) putchar0('&');
+      if ( Saki == '&') {
+        if (Replace) putchar0('&');
+        Saki = fsgetc(Inop);state = 0;return(AND); /* && */
+      }else{
+        state = 0;return('&');
+      }
+      break;
+    case 56: 
+      if (Replace) putchar0('|');
+      if ( Saki == '|') {
+        if (Replace) putchar0('|');
+        Saki = fsgetc(Inop);state = 0;return(OR); /* || */
+      }else{
+        state = 0;return('|');
+      }
+      break;
+    case 57: 
+      if (Replace) putchar0('!');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(NOTEQUAL); /* != */
+      }else{
+        state = 0;return('!');
+      }
+      break;
+    case 58: 
+      if (Replace) putchar0(':');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(PUT); /* := */
+      }else{
+        state = 0;return(':');
+      }
+      break;
+    case 59: 
+      if (Replace) putchar0('+');
+      if ( Saki == '+') {
+        if (Replace) putchar0('+');
+        Saki = fsgetc(Inop);state = 0;return(INCREMENT); /* ++ */
+      }else{
+        state = 0;return('+');
+      }
+      break;
+    case 60: 
+      if (Replace) putchar0('-');
+      if ( Saki == '-') {
+        if (Replace) putchar0('-');
+        Saki = fsgetc(Inop);state = 0;return(DECREMENT); /* -- */
+      }else if (Saki == '>') {
+        if (Replace) putchar0('>');
+        Saki = fsgetc(Inop);state = 0;return(MEMBER); /* -> */
+      }else{
+        state = 0;return('-');
+      }
+      break;
+    case 61: 
+      if (Replace) putchar0('%');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(RESIDUEPUT); /* %= */
+      }else{
+        state = 0;return('%');
+      }
+      break;
+    case 62: 
+      if (Replace) putchar0('^');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(NEGATEPUT); /* ^= */
+      }else{
+        state = 0;return('^');
+      }
+      break;
+    case 63: 
+      if (Replace) putchar0('*');
+      if ( Saki == '=') {
+        if (Replace) putchar0('=');
+        Saki = fsgetc(Inop);state = 0;return(MULTPUT); /* *= */
+      }else{
+        state = 0;return('*');
+      }
+      break;
+
+
+    default:
+      fprintf(stderr,"%d: Error in KClex().\n",Linenumber);
+    }
+  }
+
+}
+
+
+readcomment() {
+  int c;
+  while (1) {
+    c = fsgetc(Inop);
+    if (c == EOF) {
+      fprintf(stderr,"%d: Unexpected end of file in a comment.\n",Linenumber);
+      return 0;
+    }
+    if (c == '*') {
+      c = fsgetc(Inop);
+      if (c == '/') return 0;
+    }
+  }
+}
+
+readcomment2() {
+  int c;
+  while (1) {
+    c = fsgetc(Inop);
+    if (c == EOF) {
+      fprintf(stderr,"%d: Unexpected end of file in a comment.\n",Linenumber);
+      return 0;
+    }
+    if (c == '\n') {
+      return 0;
+    }
+  }
+}
+      
+
+char *readstring() {
+  static char strtmp[1024]; /* temporary */
+  static int size = 1024;
+  static char *str = strtmp;
+  int i=0;
+  int c;
+  static char *strwork;
+
+
+  if (Replace) putchar0('\"'); /* output " */
+  while (1) {
+    c = fsgetc(Inop);
+    if (Replace) putchar0(c);
+    if (c == '\"') {
+      str[i] = '\0';
+      return(str);
+    }
+    if (c == EOF) {
+      fprintf(stderr,"%d: Unexpected end of file in the string.\n",Linenumber);
+      str[i]= '\0';
+      return(str);
+    }
+    if (c == '\\') {
+      c = fsgetc(Inop);
+      if (Replace) {
+        putchar0(c);
+      }
+      if (c == EOF) {
+        fprintf(stderr,"%d: Unexpected end of file in the escape sequence.\n",Linenumber);
+        str[i]= '\0';
+        return(str);
+      }
+    }
+
+    str[i++] = c;
+    if (i >= size-10) {
+      size = size*2;
+      strwork = newString(size);
+      strcpy(strwork,str);
+      str = strwork;
+    }
+  }
+}
+      
+
+readchar() {
+  int c;
+  if (Replace) putchar0('\'');
+  c = fsgetc(Inop); /* 'c.'   '\.c' */
+  if (Replace) putchar0(c);
+  if ( c == '\\') {
+    c = fsgetc(Inop); /* '\c.' */
+    if (Replace) putchar0(c);
+    if (c == EOF) {
+      fprintf(stderr,"%d: Unexpected end of file in the escape sequence.\n",Linenumber);
+      return(-1);
+    }
+    if (fsgetc(Inop) != '\'') {
+      fprintf(stderr,"%d: Error in single quote string (escape seq)\n",Linenumber);
+      return(c);
+    }
+    if (Replace) putchar0('\'');
+    return(c);
+  }
+
+  if (fsgetc(Inop) != '\'') {
+    fprintf(stderr,"%d: Error in single quote string\n",Linenumber);
+  }
+  if (Replace) putchar0('\'');
+  return(c);
+}
+  
+putchar0(c)
+  int c;
+{
+  if (c > 0) putchar(c);
+}
+
+printf0(s)
+  char *s;
+{
+  int i = 0;
+  while (s[i] != '\0') putchar0(s[i++]);
+}
+
+printf1(s)
+  char *s;
+{
+  int i = 0;
+  /* putchar0('K'); */   /* do your own replacement */
+  while (s[i] != '\0') putchar0(s[i++]);
+}
+
+isReserved(s)
+  char *s;
+{
+  char *r[] = {"auto","break","case","char","const","continue",
+               "default","do","double","else","enum","extern",
+               "float","for","goto","if","int","long","register",
+               "return","short","signed","sizeof","static","struct",
+               "switch","typedef","union","unsigned","volatile",
+               "void","while",
+               "print","module","local","def","sm1","load","Test","special",
+               "class","super","operator","final","extends",
+               "incetanceVariable","this","new","startOfThisClass",
+               "sizeOfThisClass","PSfor","OutputPrompt"};
+           
+  int  val[]= {AUTO, BREAK, CASE, CHAR, CONST, CONTINUE, DEFAULT, DO, DOUBLE,
+               ELSE, ENUM,
+               EXTERN, FLOAT, FOR, GOTO, IF, INT, LONG, REGISTER,
+               RETURN, SHORT, SIGNED, SIZEOF, STATIC, STRUCT, SWITCH,
+               TYPEDEF, UNION,
+               UNSIGNED, VOLATILE, VOID, WHILE,
+               PRINT,MODULE,LOCAL,DEF,SM1,LOAD,TEST,SPECIAL,
+               CLASS,SUPER,OPERATOR,FINAL,EXTENDS,INCETANCEVARIABLE,
+               THIS, NEW, STARTOFTHISCLASS, SIZEOFTHISCLASS,PSFOR,PROMPT}; 
+  int  n = 52; /* Length of the list above */
+  /* You have to change simple.y, too. */
+
+  int i;
+  for (i=0; i<n; i++) {
+    if (strcmp(r[i],s) == 0) {
+      if (Debug2) printf("\nReserved word: %s ---\n",s); 
+      return(val[i]);
+    }
+  }
+  return(0);
+
+}
+
+shouldReplace(s)
+  char *s;
+{
+  char *r[] = {"dummy"};
+  int n = 1;
+  int i;
+  for (i=0; i<n; i++) {
+    if (strcmp(r[i],s) == 0) {
+      /* printf("\n--- %s ---\n",s); */
+      return(1);
+    }
+  }
+  return(1); /* change to 0. */
+}
+
+
+
+int fsgetc(FILE *fp) {
+  int c;
+  return(fgetc(fp));
+}
+
+
+int main(int argc,char *argv[]) {
+  int c;
+  if (argc <= 1) Inop = stdin;
+  else {
+    Inop = fopen(argv[1],"r");
+    if (Inop == NULL) {
+      fprintf(stderr,"File %s is not found.\n",argv[1]); return(-1);
+    }
+  }
+  while ((c=KClex()) != EOF) {
+    if (c == DEF) {
+      c=KClex();
+	  if (c != ID) {
+		fprintf(stderr,"ID (identifier) is expected, but the token type is %d\n",c);
+	  }else {
+        /* printf("ID=%s",(KClval->lc).str); */
+        printf("localf %s$\n",(KClval->lc).str); 
+      }
+    }
+  }
+  return(0);
+}
---
+++ src/util/oxlistlocalf.h
@@ -0,0 +1,130 @@
+union cell {
+  int ival;
+  char *str;
+  struct Object *op;
+  FILE *file;
+  struct stringBuf *sbuf;
+  void *voidp;
+};
+
+struct Object{
+  int tag;                /* class identifier */
+  union cell lc;          /* left cell */
+  union cell rc;          /* right cell */
+  struct object *attr;
+};
+
+struct object{         /* must be compatible with stackm.h */
+  int tag;                /* class identifier */
+  union cell lc;          /* left cell */
+  union cell rc;          /* right cell */
+  struct object *attr;
+};
+typedef struct Object * objectp;
+
+/**** data types (class identifiers) ************/
+#define Snull             0
+#define Sinteger          1     /* integer */
+#define Sstring           2     /* pointer to a string */
+#define SexecutableArray  3     /* executable array */
+#define Soperator         4     /* operators defined in the system dic */
+#define Sdollar           5     /* pointer to a string obtained from $...$ */
+#define Sarray            6     /* lc.ival is the size of array,
+				   (rc.op)[0], ..., (rc.op)[k] is the array
+				   of object */
+#define SleftBraceTag    7     /* [ */
+#define SrightBraceTag   8     /* ] */
+#define Spoly            9
+#define SarrayOfPOLY     10
+#define SmatrixOfPOLY    11
+#define Slist            12   /* list of object */
+#define Sfile            13
+#define Sring            14
+#define SuniversalNumber 15
+#define SrationalFunction 16
+#define Sclass          17   /* class, for extension */
+#define Sdouble         18
+
+#define TYPES            19   /* number of data types. */
+/* NOTE! If you change the above, you need to change mklookup.c too. */
+/* Change also dr.sm1 : datatype constants. */
+
+/* The following tags are not in stackm.h, but we use them. */
+#define CLASSNAME_CONTEXT 258
+
+
+#define ID 258
+#define QUOTE 259
+#define SINGLEQUOTE 260
+#define NUMBER 261
+#define CLASS 262
+#define SUPER 263
+#define OPERATOR 264
+#define FINAL 265
+#define EXTENDS 266
+#define INCETANCEVARIABLE 267
+#define THIS 268
+#define NEW 269
+#define SIZEOFTHISCLASS 270
+#define STARTOFTHISCLASS 271
+#define MODULE 272
+#define PRINT 273
+#define LOCAL 274
+#define DEF 275
+#define SM1 276
+#define LOAD 277
+#define TEST 278
+#define SPECIAL 279
+#define AUTO 280
+#define BREAK 281
+#define CASE 282
+#define CHAR 283
+#define CONST 284
+#define CONTINUE 285
+#define DEFAULT 286
+#define DO 287
+#define DOUBLE 288
+#define ELSE 289
+#define ENUM 290
+#define EXTERN 291
+#define FLOAT 292
+#define FOR 293
+#define GOTO 294
+#define IF 295
+#define INT 296
+#define LONG 297
+#define REGISTER 298
+#define RETURN 299
+#define SHORT 300
+#define SIGNED 301
+#define SIZEOF 302
+#define STATIC 303
+#define STRUCT 304
+#define SWITCH 305
+#define TYPEDEF 306
+#define UNION 307
+#define UNSIGNED 308
+#define VOLATILE 309
+#define VOID 310
+#define WHILE 311
+#define PSFOR 312
+#define PROMPT 313
+#define RESIDUEPUT 314
+#define NEGATEPUT 315
+#define MULTPUT 316
+#define PUT 317
+#define OR 318
+#define AND 319
+#define NOTEQUAL 320
+#define EQUAL 321
+#define GREATEREQUAL 322
+#define LESSEQUAL 323
+#define RIGHTSHIFT 324
+#define LEFTSHIFT 325
+#define DECREMENT 326
+#define INCREMENT 327
+#define UNARYMINUS 328
+#define MEMBER 329
+
+
+
