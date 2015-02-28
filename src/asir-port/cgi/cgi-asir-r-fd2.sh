0acb3fc4983846ba7f3a724d8feb09bd97840d0a
takayama <>
2015-02-28 11:39:13 +0000

    a_ahmat, r_ahmat return [Z,[[d11 Z, d12 Z, ...],[d21 Z, d22 Z, ...]] a_log_ahmat and r_log_ahmat return approximate value of [log Z, [[d11 Z/Z, d12 Z/Z, ...],[d21 Z/Z, d22 Z/Z, ...]]

--- src/asir-port/cgi/cgi-asir-r-fd2.sh
+++ src/asir-port/cgi/cgi-asir-r-fd2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# $OpenXM: OpenXM/src/asir-port/cgi/cgi-asir-r-fd2.sh,v 1.3 2014/12/12 08:29:53 takayama Exp $
+# $OpenXM: OpenXM/src/asir-port/cgi/cgi-asir-r-fd2.sh,v 1.4 2015/02/25 04:47:50 takayama Exp $
 ## Replace $OpenXM_HOME by an absolute path of your cite and uncomment
-#export OpenXM_HOME=/private/taka/OpenXM
+export OpenXM_HOME=/private/taka/OpenXM
 export HOME=/tmp
 ## Note that OpenXM_tmp is set in the dot.bashrc below as HOME/.OpenXM_tmp
 source "$OpenXM_HOME/rc/dot.bashrc"
 #source /home/taka/OpenXM/rc/dot.bashrc
 export TMP=/tmp
-export CGI_ASIR_ALLOW="[(version) (asir_contrib_copyright) (a_ahvec) (r_ahvec) (a_expect) (r_expect) ]"
+export CGI_ASIR_ALLOW="[(version) (asir_contrib_copyright) (a_ahvec) (r_ahvec) (a_expect) (r_expect) (r_ahmat) (a_ahmat) (r_log_ahmat) (a_log_ahmat)]"
 export CGI_ASIR_TLIMIT="60"
 export OX_ASIR_LOAD_FILE="$OpenXM_HOME/src/asir-port/cgi/r-fd.rr"
 export CGI_ASIR_MSG="<pre>
