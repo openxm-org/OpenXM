8fafe5ed72729062faf8ae80779623d5f668eddc
takayama <>
2015-08-27 11:03:02 +0000

    A script to fetch and install projects in outer repository.

---
+++ src/asir-contrib/outer/curl-os_muldiff.sh
@@ -0,0 +1,7 @@
+curl --verbose -u ftp:takayama@math.kobe-u.ac.jp -O ftp://akagi.ms.u-tokyo.ac.jp/pub/math/muldif/os_muldif.rr
+curl --verbose -u ftp:takayama@math.kobe-u.ac.jp -O ftp://akagi.ms.u-tokyo.ac.jp/pub/math/muldif/3DGraph.pdf
+curl --verbose -u ftp:takayama@math.kobe-u.ac.jp -O ftp://akagi.ms.u-tokyo.ac.jp/pub/math/muldif/mtoupper.pdf
+curl --verbose -u ftp:takayama@math.kobe-u.ac.jp -O ftp://akagi.ms.u-tokyo.ac.jp/pub/math/muldif/os_muldif.pdf
+
+
+
