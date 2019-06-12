9cd9409abaac57755d7f6d8d6f57b8ae2bd49f3c
takayama <>
2019-06-12 06:14:20 +0000

    use-asir-mode.sh checks if .emacs.d/init.el is loaded.

--- src/asir-mode/use-asir-mode.sh
+++ src/asir-mode/use-asir-mode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $OpenXM: OpenXM/src/asir-mode/use-asir-mode.sh,v 1.1 2016/02/12 02:39:26 takayama Exp $
+# $OpenXM: OpenXM/src/asir-mode/use-asir-mode.sh,v 1.2 2016/02/12 02:43:33 takayama Exp $
 
 _ok_local() {
 	echo "------------------------------------------------------------------"
@@ -22,10 +22,24 @@ _install_local() {
 	cat $OpenXM_HOME/share/emacs/use-asir-mode-local.txt >>$HOME/.emacs.d/init.el
 }
 
+_check() {
+    if [ -f $HOME/.emacs ]; then
+	grep 'emacs\.d/init\.el' $HOME/.emacs ;
+	if [ $? -eq 0 ]; then
+	    echo "OK ...";
+	else
+	    echo "Warning! init.el is not loaded from .emacs. Check if you load init.el";
+	    echo "  Example:  Add the following line ";
+	    echo "       (load \"~/.emacs.d/init.el\") ";
+	    echo "  to $HOME/.emacs";
+        fi
+    fi
+}
+
 _install() {
 	cat $OpenXM_HOME/share/emacs/use-asir-mode-local.txt >>$HOME/.emacs.d/init.el
 }
-
+_check ;
 if [ $# = 0 ]; then
 	echo "Usage: use-asir-mode.sh --local" ; \
 	echo "       use-asir-mode.sh --local-yes" ; \
