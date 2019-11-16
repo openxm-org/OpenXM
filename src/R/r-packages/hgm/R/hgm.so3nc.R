6364a813198d0997bb1aaa249f0c5595212fea6f
takayama <>
2019-11-16 11:03:44 +0000

    R package for the new hgm.ncso3(log=1) in the previous commit.

--- src/R/r-packages/hgm/R/hgm.so3nc.R
+++ src/R/r-packages/hgm/R/hgm.so3nc.R
@@ -1,10 +1,12 @@
-# $OpenXM: OpenXM/src/R/r-packages/hgm/R/hgm.so3nc.R,v 1.4 2013/03/08 07:32:28 takayama Exp $
+# $OpenXM: OpenXM/src/R/r-packages/hgm/R/hgm.so3nc.R,v 1.5 2014/03/22 00:15:40 takayama Exp $
 "hgm.tk.so3nc" <-
-function(a=1,b=2,c=3,t0=0.0,q=1,deg=0) { 
+function(a=1,b=2,c=3,t0=0.0,q=1,deg=0,log=0) { 
 #  library.dynam("mypkg",package="mypkg",lib.loc="./");
   if (!is.loaded("hgm")) library.dynam("hgm",package="hgm",lib.loc=NULL);
+  ans <-
   .C("so3_main",as.double(a),as.double(b),as.double(c),
      as.double(t0),
-     as.integer(q),as.integer(deg),
-     retv=as.double(0),PACKAGE="hgm")$retv
+     as.integer(q),as.integer(deg),as.integer(log),
+     retv=double(4),PACKAGE="hgm")$retv;
+  return(ans); 
 }
