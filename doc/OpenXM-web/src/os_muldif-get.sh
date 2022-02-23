2780c46c582cd259db45d6565ba4f4c5c1040645
takayama <>
2022-02-23 02:09:20 +0000

    Remove tags in os_muldif.rr orig.

--- doc/OpenXM-web/src/os_muldif-get.sh
+++ doc/OpenXM-web/src/os_muldif-get.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $OpenXM: OpenXM/doc/OpenXM-web/src/os_muldif-get.sh,v 1.2 2017/06/12 04:29:12 takayama Exp $
+# $OpenXM: OpenXM/doc/OpenXM-web/src/os_muldif-get.sh,v 1.3 2018/12/10 05:46:22 takayama Exp $
 OpenXM_HOME=/home/taka/OX4/OX64/OpenXM
 #OpenXM_HOME=/home/nobuki/OX4/OpenXM
 OX_MULDIF=${OpenXM_HOME}/src/asir-contrib/packages/src/os_muldif.rr
@@ -11,7 +11,7 @@ if true ; then
 (cd muldif ; curl http://www.ms.u-tokyo.ac.jp/~oshima/muldif/os_muldif.dvi >os_muldif.dvi)
 fi
 head -1 ${OX_MULDIF} >muldif/tmp-os_muldif.rr
-cat muldif/os_muldif.rr >>muldif/tmp-os_muldif.rr
+sed -e '/$OpenXM.*takayama/d' muldif/os_muldif.rr >>muldif/tmp-os_muldif.rr
 if diff -B -b -q muldif/tmp-os_muldif.rr ${OX_MULDIF} >/dev/null ; then
   echo "there is no change in os_muldif.rr"
 else
